9dfbdc3ef94075ab700e283341161c74887c29fa
MTibby92 <MTibby92@gmail.com>
2016-12-16 12:02:28 -0600

    need to refactor the way that saved articles are presented; currently only works with 5 entries saved

--- app/components/Saved.js
+++ app/components/Saved.js
@@ -19,6 +19,42 @@ var Saved = React.createClass({
 								</div>
 							</div>
 						</div>
+						<div className="well well-sm">
+							<div className="row">
+								<div className="col-sm-10">This is where a Saved Article would appear</div>
+								<div className="col-sm-2">
+									<button  
+										className="btn btn-sm btn-block btn-warn">Delete</button>
+								</div>
+							</div>
+						</div>
+						<div className="well well-sm">
+							<div className="row">
+								<div className="col-sm-10">This is where a Saved Article would appear</div>
+								<div className="col-sm-2">
+									<button  
+										className="btn btn-sm btn-block btn-warn">Delete</button>
+								</div>
+							</div>
+						</div>
+						<div className="well well-sm">
+							<div className="row">
+								<div className="col-sm-10">This is where a Saved Article would appear</div>
+								<div className="col-sm-2">
+									<button  
+										className="btn btn-sm btn-block btn-warn">Delete</button>
+								</div>
+							</div>
+						</div>
+						<div className="well well-sm">
+							<div className="row">
+								<div className="col-sm-10">This is where a Saved Article would appear</div>
+								<div className="col-sm-2">
+									<button  
+										className="btn btn-sm btn-block btn-warn">Delete</button>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			)
@@ -38,6 +74,62 @@ var Saved = React.createClass({
 										data-title={this.props.savedArticles[0].title}
 										data-published={this.props.savedArticles[0].published}
 										data-url={this.props.savedArticles[0].url}
+										onClick={this.props.onArticleSave} 
+										className="btn btn-sm btn-block btn-danger">Delete</button>
+								</div>
+							</div>
+						</div>
+						<div className="well well-sm">
+							<div className="row">
+								<div className="col-sm-10">{this.props.savedArticles[1].title}</div>
+								<div className="col-sm-2">
+									<button 
+										// id="button1" 
+										data-title={this.props.savedArticles[1].title}
+										data-published={this.props.savedArticles[1].published}
+										data-url={this.props.savedArticles[1].url}
+										onClick={this.props.onArticleSave} 
+										className="btn btn-sm btn-block btn-danger">Delete</button>
+								</div>
+							</div>
+						</div>
+						<div className="well well-sm">
+							<div className="row">
+								<div className="col-sm-10">{this.props.savedArticles[2].title}</div>
+								<div className="col-sm-2">
+									<button 
+										// id="button1" 
+										data-title={this.props.savedArticles[2].title}
+										data-published={this.props.savedArticles[2].published}
+										data-url={this.props.savedArticles[2].url}
+										onClick={this.props.onArticleSave} 
+										className="btn btn-sm btn-block btn-danger">Delete</button>
+								</div>
+							</div>
+						</div>
+						<div className="well well-sm">
+							<div className="row">
+								<div className="col-sm-10">{this.props.savedArticles[3].title}</div>
+								<div className="col-sm-2">
+									<button 
+										// id="button1" 
+										data-title={this.props.savedArticles[3].title}
+										data-published={this.props.savedArticles[3].published}
+										data-url={this.props.savedArticles[3].url}
+										onClick={this.props.onArticleSave} 
+										className="btn btn-sm btn-block btn-danger">Delete</button>
+								</div>
+							</div>
+						</div>
+						<div className="well well-sm">
+							<div className="row">
+								<div className="col-sm-10">{this.props.savedArticles[4].title}</div>
+								<div className="col-sm-2">
+									<button 
+										// id="button1" 
+										data-title={this.props.savedArticles[4].title}
+										data-published={this.props.savedArticles[4].published}
+										data-url={this.props.savedArticles[4].url}
 										onClick={this.props.onArticleSave} 
 										className="btn btn-sm btn-block btn-danger">Delete</button>
 								</div>
