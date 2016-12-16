de4028c8baff9a2cf727dfa41c88641ce3981778
MTibby92 <MTibby92@gmail.com>
2016-12-15 19:26:45 -0600

    added UI for save buttons on results

--- app/components/Results.js
+++ app/components/Results.js
@@ -11,31 +11,109 @@ var Results = React.createClass({
 
 		if (this.props.results.length == 0) {
 			return (
-				<div className="panel panel-default">
-				<div className="panel-heading text-center">
-					<h3 className="panel-title">Results</h3>
+				<div className="row">
+					<div className="col-sm-12">
+						<div className="panel panel-default">
+							<div className="panel-heading text-center">
+								<h3 className="panel-title">Results</h3>
+							</div>
+							<div className="panel-body">
+								<div className="well well-sm">
+									<div className="row">
+										<div className="col-sm-10">Result 1</div>
+										<div className="col-sm-2">
+											<button className="btn btn-sm btn-block btn-primary">Save</button>
+										</div>
+									</div>
+								</div>
+								<div className="well well-sm">
+									<div className="row">
+										<div className="col-sm-10">Result 2</div>
+										<div className="col-sm-2">
+											<button className="btn btn-sm btn-block btn-primary">Save</button>
+										</div>
+									</div>
+								</div>
+								<div className="well well-sm">
+									<div className="row">
+										<div className="col-sm-10">Result 3</div>
+										<div className="col-sm-2">
+											<button className="btn btn-sm btn-block btn-primary">Save</button>
+										</div>
+									</div>
+								</div>
+								<div className="well well-sm">
+									<div className="row">
+										<div className="col-sm-10">Result 4</div>
+										<div className="col-sm-2">
+											<button className="btn btn-sm btn-block btn-primary">Save</button>
+										</div>
+									</div>
+								</div>
+								<div className="well well-sm">
+									<div className="row">
+										<div className="col-sm-10">Result 5</div>
+										<div className="col-sm-2">
+											<button className="btn btn-sm btn-block btn-primary">Save</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div className="panel-body">
-					<div className="well well-sm">Result 1</div>
-					<div className="well well-sm">Result 2</div>
-					<div className="well well-sm">Result 3</div>
-					<div className="well well-sm">Result 4</div>
-					<div className="well well-sm">Result 5</div>
-				</div>
-			</div>
 			)
 		} else {
 			return (
-				<div className="panel panel-default">
-					<div className="panel-heading text-center">
-						<h3 className="panel-title">Results</h3>
-					</div>
-					<div className="panel-body">
-						<div className="well well-sm">{this.props.results[0].headline.main}</div>
-						<div className="well well-sm">{this.props.results[1].headline.main}</div>
-						<div className="well well-sm">{this.props.results[2].headline.main}</div>
-						<div className="well well-sm">{this.props.results[3].headline.main}</div>
-						<div className="well well-sm">{this.props.results[4].headline.main}</div>
+				<div className="row">
+					<div className="col-sm-12">
+						<div className="panel panel-default">
+							<div className="panel-heading text-center">
+								<h3 className="panel-title">Results</h3>
+							</div>
+							<div className="panel-body">
+								<div className="well well-sm">
+									<div className="row">
+										<div className="col-sm-10">{this.props.results[0].headline.main}</div>
+										<div className="col-sm-2">
+											<button className="btn btn-sm btn-block btn-primary">Save</button>
+										</div>
+									</div>
+								</div>
+								<div className="well well-sm">
+									<div className="row">
+										<div className="col-sm-10">{this.props.results[1].headline.main}</div>
+										<div className="col-sm-2">
+											<button className="btn btn-sm btn-block btn-primary">Save</button>
+										</div>
+									</div>
+								</div>
+								<div className="well well-sm">
+									<div className="row">
+										<div className="col-sm-10">{this.props.results[2].headline.main}</div>
+										<div className="col-sm-2">
+											<button className="btn btn-sm btn-block btn-primary">Save</button>
+										</div>
+									</div>
+								</div>
+								<div className="well well-sm">
+									<div className="row">
+										<div className="col-sm-10">{this.props.results[3].headline.main}</div>
+										<div className="col-sm-2">
+											<button className="btn btn-sm btn-block btn-primary">Save</button>
+										</div>
+									</div>
+								</div>
+								<div className="well well-sm">
+									<div className="row">
+										<div className="col-sm-10">{this.props.results[4].headline.main}</div>
+										<div className="col-sm-2">
+											<button className="btn btn-sm btn-block btn-primary">Save</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			)
