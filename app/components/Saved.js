89e6075a828251b4f5533eead048ba8b17dbb788
MTibby92 <MTibby92@gmail.com>
2016-12-16 15:08:42 -0600

    refactored Save component to render based on number of articles in database

--- app/components/Saved.js
+++ app/components/Saved.js
@@ -3,142 +3,34 @@ var PropTypes = React.PropTypes
 
 var Saved = React.createClass({
 	render: function() {
-		if (this.props.savedArticles.length == 0) {
-			return (
-				<div className="panel panel-default">
-					<div className="panel-heading text-center">
-						<h3 className="panel-title">Saved Container</h3>
-					</div>
-					<div className="panel-body">
-						<div className="well well-sm">
-							<div className="row">
-								<div className="col-sm-10">This is where a Saved Article would appear</div>
-								<div className="col-sm-2">
-									<button  
-										className="btn btn-sm btn-block btn-warn">Delete</button>
-								</div>
-							</div>
-						</div>
-						<div className="well well-sm">
-							<div className="row">
-								<div className="col-sm-10">This is where a Saved Article would appear</div>
-								<div className="col-sm-2">
-									<button  
-										className="btn btn-sm btn-block btn-warn">Delete</button>
-								</div>
-							</div>
-						</div>
-						<div className="well well-sm">
-							<div className="row">
-								<div className="col-sm-10">This is where a Saved Article would appear</div>
-								<div className="col-sm-2">
-									<button  
-										className="btn btn-sm btn-block btn-warn">Delete</button>
-								</div>
-							</div>
-						</div>
-						<div className="well well-sm">
-							<div className="row">
-								<div className="col-sm-10">This is where a Saved Article would appear</div>
-								<div className="col-sm-2">
-									<button  
-										className="btn btn-sm btn-block btn-warn">Delete</button>
-								</div>
-							</div>
-						</div>
-						<div className="well well-sm">
-							<div className="row">
-								<div className="col-sm-10">This is where a Saved Article would appear</div>
-								<div className="col-sm-2">
-									<button  
-										className="btn btn-sm btn-block btn-warn">Delete</button>
-								</div>
-							</div>
-						</div>
-					</div>
+		return (
+			<div className="panel panel-default">
+				<div className="panel-heading text-center">
+					<h3 className="panel-title">Saved Container</h3>
 				</div>
-			)
-		} else {
-			return (
-				<div className="panel panel-default">
-					<div className="panel-heading text-center">
-						<h3 className="panel-title">Saved Container</h3>
-					</div>
-					<div className="panel-body">
+				<div className="panel-body">
+
+				{this.props.savedArticles.map(function(article, num) {
+					return (
 						<div className="well well-sm">
 							<div className="row">
-								<div className="col-sm-10">{this.props.savedArticles[0].title}</div>
+								<div className="col-sm-10">{article.title}</div>
 								<div className="col-sm-2">
 									<button 
 										// id="button1" 
-										data-title={this.props.savedArticles[0].title}
-										data-published={this.props.savedArticles[0].published}
-										data-url={this.props.savedArticles[0].url}
+										data-title={article.title}
+										data-published={article.published}
+										data-url={article.url}
 										onClick={this.props.onArticleSave} 
 										className="btn btn-sm btn-block btn-danger">Delete</button>
 								</div>
 							</div>
 						</div>
-						<div className="well well-sm">
-							<div className="row">
-								<div className="col-sm-10">{this.props.savedArticles[1].title}</div>
-								<div className="col-sm-2">
-									<button 
-										// id="button1" 
-										data-title={this.props.savedArticles[1].title}
-										data-published={this.props.savedArticles[1].published}
-										data-url={this.props.savedArticles[1].url}
-										onClick={this.props.onArticleSave} 
-										className="btn btn-sm btn-block btn-danger">Delete</button>
-								</div>
-							</div>
-						</div>
-						<div className="well well-sm">
-							<div className="row">
-								<div className="col-sm-10">{this.props.savedArticles[2].title}</div>
-								<div className="col-sm-2">
-									<button 
-										// id="button1" 
-										data-title={this.props.savedArticles[2].title}
-										data-published={this.props.savedArticles[2].published}
-										data-url={this.props.savedArticles[2].url}
-										onClick={this.props.onArticleSave} 
-										className="btn btn-sm btn-block btn-danger">Delete</button>
-								</div>
-							</div>
-						</div>
-						<div className="well well-sm">
-							<div className="row">
-								<div className="col-sm-10">{this.props.savedArticles[3].title}</div>
-								<div className="col-sm-2">
-									<button 
-										// id="button1" 
-										data-title={this.props.savedArticles[3].title}
-										data-published={this.props.savedArticles[3].published}
-										data-url={this.props.savedArticles[3].url}
-										onClick={this.props.onArticleSave} 
-										className="btn btn-sm btn-block btn-danger">Delete</button>
-								</div>
-							</div>
-						</div>
-						<div className="well well-sm">
-							<div className="row">
-								<div className="col-sm-10">{this.props.savedArticles[4].title}</div>
-								<div className="col-sm-2">
-									<button 
-										// id="button1" 
-										data-title={this.props.savedArticles[4].title}
-										data-published={this.props.savedArticles[4].published}
-										data-url={this.props.savedArticles[4].url}
-										onClick={this.props.onArticleSave} 
-										className="btn btn-sm btn-block btn-danger">Delete</button>
-								</div>
-							</div>
-						</div>
-					</div>
+					)
+				}.bind(this))}
 				</div>
-			)
-		}	
+			</div>
+		)
 	}
 })
 
